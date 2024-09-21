ddb04e6cdd85df7da9a929cdf8b2ab09fa56b364
pilluaws <pilluaws@gmail.com>
2024-09-21 16:55:54 +0100

    Update mathHelper.py- Added comment

--- src/mathHelper.py
+++ src/mathHelper.py
@@ -11,6 +11,6 @@ def calculate_sin(angle):
 
 def calculate_cos(angle):
     return math.cos(math.radians(angle))
-
+##coments added
 def distance(point1, point2):
     return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
